96d4482495cfe842afa82b3a9cfc94965cbf1df8
gpay <gpaytakov@gmail.com>
2022-04-29 22:45:23 -0500

    create a votes table

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-// importing modules
-const db = require('./db/connection');
 const express = require('express');
-const inputCheck = require('./utils/inputCheck');
+const db = require('./db/connection');
 const apiRoutes = require('./routes/apiRoutes');
 
 const PORT = process.env.PORT || 3001;
@@ -14,7 +12,7 @@ app.use(express.json());
 // Use apiRoutes
 app.use('/api', apiRoutes);
 
-// Not Found response for unmatched routes
+// Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
